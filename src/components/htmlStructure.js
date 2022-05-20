f33ddfc542599101f96547d25eb353edb72d7699
Shalane Proctor <shalane088@gmail.com>
2022-05-19 19:24:00 -0500

    added styling elements

--- src/components/htmlStructure.js
+++ src/components/htmlStructure.js
@@ -3,18 +3,27 @@ import renderToDom from '../scripts/helpers/renderToDom';
 // HTML STRUCTURE
 const app = () => {
   const domstring = `<div id="main">
-  <h1>Get Lyrics!🎵 🎶</h1>
-  
-  <form>
-  <div class="input-group mb-3">
-  <span class="input-group-text" id="inputGroup-sizing-default">Artist</span>
-  <input id="artistInput" type="text" class="form-control" required>
+  <h1>What are the lyrics?</h1>
+<form>
+<div class="d-flex justify-content-center align-items-center container ">
+  <div id="inpuntContainer" class="row">
+    <div class="col md">
+      <div class="form-floating">
+        <input id="artistInput" type="text" class="form-control">
+        <label for="artistInput">Artist</label>
+      </div>
+    </div>
+  <div class="col">
+    <div class="form-floating">
+      <input id="songInput"  type="text" class="form-control">
+      <label for="songInput">Song</label>
+    </div>
+  </div>
 </div>
-<div  class="input-group mb-3">
-  <span class="input-group-text" id="inputGroup-sizing-default">Song</span>
-  <input id="songInput" type="text" class="form-control" required>
 </div>
-<button id="postLyrics" type="submit" class="btn btn-info">Get Lyrics</button>
+<div id="buttonContainer">
+<button id="postLyrics" type="submit" class="btn btn-info">Get lyrics</button>
+</div>
 </form>
   <div id="lyricsDiv"></div>
   </div>`;
